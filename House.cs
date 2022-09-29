91aea45e37573818609449c6cb2aec12de68f9e6
hamp13773 <105466669+hamp13773@users.noreply.github.com>
2022-09-29 16:39:08 +0300

    Update House.cs

--- House.cs
+++ House.cs
@@ -1,60 +1,38 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-
 public class House : MonoBehaviour
 {
-    private readonly float recoveryRate = 0.2f;
+    [SerializeField] private AudioSource _audioSource;
     private readonly float _minVolume = 0;
     private readonly float _maxVolume = 1;
-    private bool _isProvoke = false;
-
-    private AudioSource _audioSource;
+    private readonly float recoveryRate = 0.0002f;
 
     private void OnEnable()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.Play();
     }
-
-    private void Update()
-    {
-        if (_isProvoke)
-        {
-            StartCoroutine(RaiseVolume());
-        }
-
-        else
-        {
-            StartCoroutine(MinimaseVolume());
-        }
-    }
-
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Thief>(out Thief thief))
         {
-            _isProvoke = true;
+            StartCoroutine(ChangeVolume(_maxVolume));
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _isProvoke = false;
+        StopCoroutine(ChangeVolume(_maxVolume));
+        StartCoroutine(ChangeVolume(_minVolume));
     }
 
-    private IEnumerator MinimaseVolume()
+    public IEnumerator ChangeVolume(float volumeToChange)
     {
-        StopCoroutine(RaiseVolume());
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, recoveryRate * Time.deltaTime);
-        yield return null;
-    }
-
-    private IEnumerator RaiseVolume()
-    {
-        StopCoroutine(MinimaseVolume());
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, recoveryRate * Time.deltaTime);
-        yield return null;
+        while (_audioSource.volume != volumeToChange)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, volumeToChange, recoveryRate);
+            yield return null;
+        }
     }
 }
