59d7d00e79136a3b689c929c8c1991fd9d452701
hamp13773 <105466669+hamp13773@users.noreply.github.com>
2022-09-30 18:38:05 +0300

    1

--- Alarm.cs
+++ Alarm.cs
@@ -7,16 +7,36 @@ using UnityEngine;
 public class Alarm : MonoBehaviour
 {
     private AudioSource _audioSource;
-    private readonly float recoveryRate = 0.0002f;
+    private House _house;
+    private readonly float recoveryRate = 0.002f;
+    private readonly float _minVolume = 0;
+    private readonly float _maxVolume = 1;
     private void OnEnable()
     {
         _audioSource = GetComponent<AudioSource>();
+        _house = GetComponent<House>();
     }
-    public IEnumerator ChangeVolume(float volumeToChange)
+
+    private void Update()
+    {
+        if (_house.IsEnter == true)
+        {
+            StopCoroutine(ChangeVolume(_minVolume));
+            StartCoroutine(ChangeVolume(_maxVolume));
+        }
+
+        else
+        { 
+            StopCoroutine(ChangeVolume(_maxVolume));
+            StartCoroutine(ChangeVolume(_minVolume));
+        }
+    }
+
+    private IEnumerator ChangeVolume(float volumeToChange)
     {
         while (_audioSource.volume != volumeToChange)
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, volumeToChange, recoveryRate);
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, volumeToChange, recoveryRate * Time.deltaTime);
             yield return null;
         }
     }
--- House.cs
+++ House.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 public class House : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private Alarm _alarm;
-    private readonly float _minVolume = 0;
-    private readonly float _maxVolume = 1;
+
+    public bool IsEnter { get; private set; }
 
     private void OnEnable()
     {
         _audioSource = GetComponent<AudioSource>();
-        _alarm = GetComponent<Alarm>();
         _audioSource.Play();
+        IsEnter = false;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Thief>(out Thief thief))
         {
-            StartCoroutine(_alarm.ChangeVolume(_maxVolume));
+            IsEnter = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        StopCoroutine(_alarm.ChangeVolume(_maxVolume));
-        StartCoroutine(_alarm.ChangeVolume(_minVolume));
+        IsEnter = false;
     }
 }
